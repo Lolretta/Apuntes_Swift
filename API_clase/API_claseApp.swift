c01eeab574de4a0d9beab703b261cf90f21a0fe1
lolretta <jose.retta@iest.edu.mx>
2025-11-05 18:38:32 -0600

    Iniciando Proyecto

---
+++ API_clase/API_claseApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_claseApp.swift
+//  API_clase
+//
+//  Created by Lolretta on 05/11/25.
+//
+
+import SwiftUI
+
+@main
+struct API_claseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
